6c6b61062afe44648d47bc88bf4b866110ab4c91
Loicp83 <loic.pilon@isen.yncrea.fr>
2021-01-09 15:13:21 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,55 +1,59 @@
-#include"fonction.h"
+#include "fonction.h"
 
-	
 int main(int argc, char **argv)
 {
 	initialiseGfx(argc, argv);
 	prepareFenetreGraphique("GfxLib", LargeurFenetre, HauteurFenetre);
 	lanceBoucleEvenements();
-	
+
 	return 0;
 }
 
 void gestionEvenement(EvenementGfx evenement)
 {
-	
+
 	switch (evenement)
 	{
-		case Initialisation:
-			demandeTemporisation(20);
-			break;
-		
-		case Temporisation:
-			rafraichisFenetre();
-			break;
-			
-		case Affichage:
-			effaceFenetre (255, 255, 255);
-			break;
-			
-		case Clavier:
-			break;
-			
-		case ClavierSpecial:
-			break;
+	case Initialisation:
+		demandeTemporisation(20);
+		break;
 
-		case BoutonSouris:
-			if (etatBoutonSouris() == GaucheAppuye)
-			{
-				
-			}
+	case Temporisation:
+		rafraichisFenetre();
+		break;
+
+	case Affichage:
+		effaceFenetre(255, 255, 255);
+		break;
+
+	case Clavier:
+		switch (caractereClavier())
+		{
+		case 'Q':
+		case 'q':
+			termineBoucleEvenements();
 			break;
-		
-		case Souris:
-			break;
-		
-		case Inactivite: 
-			break;
-		
-		case Redimensionnement: 
-			//printf("Largeur : %d\t", largeurFenetre());
-			//printf("Hauteur : %d\n", hauteurFenetre());
-			break;
+		}
+		break;
+
+	case ClavierSpecial:
+		break;
+
+	case BoutonSouris:
+		switch (etatBoutonSouris() == GaucheAppuye)
+		{
+		}
+		break;
+
+	case Souris:
+		break;
+
+	case Inactivite:
+		break;
+
+	case Redimensionnement:
+		//printf("Largeur : %d\t", largeurFenetre());
+		//printf("Hauteur : %d\n", hauteurFenetre());
+		break;
 	}
 }
-
