efd49431a4f93e7b6289780d315087979e5dee7b
Mailliw83 <45948985+Mailliw83@users.noreply.github.com>
2021-01-19 11:03:44 +0100

    code V0.05

--- Code/Affichage/affichage.c
+++ Code/Affichage/affichage.c
@@ -2,6 +2,7 @@
 
 void pageAccueil() //Affichage page d'accueil
 {
+    epaisseurDeTrait(3);
     couleurCourante(255, 255, 255);
     rectangle(3 * largeurFenetre() / 4, hauteurFenetre() / 12, 15 * largeurFenetre() / 16, hauteurFenetre() / 6);              //Quitter
     rectangle(5 * largeurFenetre() / 16, 16 * hauteurFenetre() / 24, 11 * largeurFenetre() / 16, 19 * hauteurFenetre() / 24);  //jouer
@@ -20,7 +21,9 @@ void pageAccueil() //Affichage page d'accueil
 
 void pageJeu()
 {
-    couleurCourante(255, 255, 255);
+    srand(clock());
+    epaisseurDeTrait(3);
+    couleurCourante(rand() % 255, rand() % 255, rand() % 255);
     rectangle(9 * largeurFenetre() / 32, 16 * hauteurFenetre() / 24, 24 * largeurFenetre() / 32, 19 * hauteurFenetre() / 24); //JcJ
     rectangle(9 * largeurFenetre() / 32, hauteurFenetre() / 2, 24 * largeurFenetre() / 32, 15 * hauteurFenetre() / 24);       //contreIA
     rectangle(9 * largeurFenetre() / 32, hauteurFenetre() / 3, 24 * largeurFenetre() / 32, 11 * hauteurFenetre() / 24);       //entrainement
@@ -37,16 +40,23 @@ void pageJeu()
     afficheChaine("Jouer contre IA", 30, 5 * largeurFenetre() / 16, 13 * hauteurFenetre() / 24);
     afficheChaine("Entrainement", 30, 5 * largeurFenetre() / 16, 9 * hauteurFenetre() / 24);
     afficheChaine("Retour", 30, 31 * largeurFenetre() / 40, hauteurFenetre() / 10);
+
+    couleurCourante(rand() % 255, rand() % 255, rand() % 255);
+    epaisseurDeTrait(8);
+    afficheChaine("PONG", 60, 13 * largeurFenetre() / 32, 21 * hauteurFenetre() / 24);
 }
 
 void pageRegles()
 {
+    epaisseurDeTrait(3);
     couleurCourante(255, 255, 255);
     rectangle(2 * largeurFenetre() / 8, 9 * hauteurFenetre() / 12, 3 * largeurFenetre() / 4, 11 * hauteurFenetre() / 12); //titre
     rectangle(3 * largeurFenetre() / 4, hauteurFenetre() / 12, 15 * largeurFenetre() / 16, hauteurFenetre() / 6);         //Retour
+
     couleurCourante(0, 0, 0);
     rectangle(2 * largeurFenetre() / 8 + 1, 9 * hauteurFenetre() / 12 + 1, 3 * largeurFenetre() / 4 - 1, 11 * hauteurFenetre() / 12 - 1); //titre
     rectangle(3 * largeurFenetre() / 4 + 1, hauteurFenetre() / 12 + 1, 15 * largeurFenetre() / 16 - 1, hauteurFenetre() / 6 - 1);         //Retour
+    epaisseurDeTrait(2);
     couleurCourante(255, 255, 255);
     afficheChaine("Retour", 30, 31 * largeurFenetre() / 40, hauteurFenetre() / 10);
     afficheChaine("Reglement", 60, 5 * largeurFenetre() / 16, 19 * hauteurFenetre() / 24);
@@ -55,5 +65,3 @@ void pageRegles()
     afficheChaine("a l'aide de raquettes, le joueurs ayant le plus de points", 20, largeurFenetre() / 10, 5 * hauteurFenetre() / 12);
     afficheChaine("gagne la partie", 20, largeurFenetre() / 10, 4 * hauteurFenetre() / 12);
 }
-
-
