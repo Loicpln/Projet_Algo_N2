7c0c27ace80f216bb41237108eb7d315044c1254
Loicp83 <loic.pilon@isen.yncrea.fr>
2021-01-27 19:41:40 +0100

    ah!

--- Code/main.c
+++ Code/main.c
@@ -22,14 +22,11 @@ void gestionEvenement(EvenementGfx evenement)
 	static Data d;
 	Data *data = &d;
 
-	FMOD_SYSTEM *system;
 
 	switch (evenement)
 	{
 	case Initialisation:
 		d = init();
-		FMOD_System_Create(&system);
-		FMOD_System_Init(system,2,FMOD_INIT_NORMAL,NULL);
 		demandeTemporisation(20);
 		break;
 
